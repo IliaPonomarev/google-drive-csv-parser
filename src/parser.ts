a8df799286d97adfda4732565fa4989abf1393e9
IliaPonomarev <seikadi123@gmail.com>
2021-08-08 22:34:33 +0300

    Fixed missing records when saving to mongodb

--- src/parser.ts
+++ src/parser.ts
@@ -1,26 +1,29 @@
 import * as csvParser from 'csv-parser';
+const MAX_OBJECT_COUNT = 1000;
 
 export function parseCSV(res, callback): Promise<boolean> {
   let temp = [];
 
   return new Promise((resolve) => {
     res
-      .on('data', async () => {
-        res.pause();
-
-        if (temp.length > 0) {
-          await callback(temp);
-
-          temp = [];
-        }
-
-        res.resume();
-      })
       .pipe(csvParser())
       .on('data', async (data) => {
         temp.push(data);
+
+        if (temp.length > MAX_OBJECT_COUNT) {
+          res.pause();
+          const infoForSave = [...temp];
+          temp = [];
+
+          await callback(infoForSave);
+
+          res.resume();
+        }
       })
-      .on('end', () => {
+      .on('end', async () => {
+        await callback(temp);
+
+        temp = [];
         resolve(true);
       });
   });
